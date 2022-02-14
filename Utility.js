387b7ab0f681b45448da2273f9605cd2908b5b7f
Luka <ivanovic@tutanota.de>
2022-02-14 13:02:58 +0100

    Initial commit

---
+++ Utility.js
@@ -0,0 +1,46 @@
+const [UP,RIGHT,DOWN,LEFT] = [1,2,3,4];
+const [GRAY,OCHRE,WHITE,BLACK,LIGHT_GRAY] =
+      ["#8f99a8","#d9b041","#ffffff","#000000","#b5b3ac"];
+
+function GetBestScore() {
+  let best_score = localStorage.getItem("best score");
+  return Number(best_score);
+}
+
+function SetBestScore(best_score) {
+  localStorage.setItem("best score",best_score);
+}
+
+function ClearBestScore() {
+  localStorage.removeItem("best score");
+}
+
+function RandomInt(min,max) {
+  // returns x from [min,max)
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function GetNeighbours(board,x,y) {
+  let neighbours = [];
+  if (x==0){
+    if (y>0)
+      neighbours.push(board[x][y-1]);
+    if (y<3)
+      neighbours.push(board[x][y+1])
+    neighbours.push(board[x+1][y]);
+  }else if (x==3){
+    if (y>0)
+      neighbours.push(board[x][y-1]);
+    if (y<3)
+      neighbours.push(board[x][y+1])
+    neighbours.push(board[x-1][y]);
+  }else{
+    if (y>0)
+      neighbours.push(board[x][y-1]);
+    if (y<3)
+      neighbours.push(board[x][y+1])
+    neighbours.push(board[x-1][y]);
+    neighbours.push(board[x+1][y]);
+  }
+  return neighbours;
+}
